7d350b848ea8b9580ee70f2c807e1defac1a62c1
cbastian3 <71797321+cbastian3@users.noreply.github.com>
2020-12-10 17:20:48 -0800

    Final version for class

--- js/working_final.js
+++ js/working_final.js
@@ -1,11 +1,10 @@
-// these need to be accessed inside more than one function so we'll declare them first
+//essential three.js variables
 let container;
 let camera;
 let controls;
 let renderer;
 let scene;
-
-//audio variables brought in from visualizer.html
+//audio variables
 let fftSize = 128;
 let listener = new THREE.AudioListener();
 let audio = new THREE.Audio( listener );
@@ -15,32 +14,24 @@ let mediaElement3 = new Audio( 'audio/Modular3RhodesChorus_1.mp3' );
 let mediaElement4 = new Audio( 'audio/Modular1Rhodes_1.mp3' );
 let mediaElement5 = new Audio( 'audio/Modular2Synth_1.mp3' );
 let mediaElement6 = new Audio( 'audio/Modular3RhodesChorus_1.mp3' );
-
-
 let analyser = new THREE.AudioAnalyser( audio, fftSize );
-
 //sphere_one
-var geometry = new THREE.SphereGeometry( 5, 5, 25 );
-var material = new THREE.MeshLambertMaterial( { color: "0000FF", opacity:0.5, transparent:true, wireframe:true, emissive: 0x0000FF,emissiveIntensity:0.6} );
+var geometry = new THREE.SphereGeometry( 13, 15, 25 );
+var material = new THREE.MeshLambertMaterial( { color: "#00FF00", opacity:1, transparent:true, wireframe:true, emissive: 0x0000FF,emissiveIntensity:2.5} );
 var sphere_one = new THREE.Mesh( geometry, material );
 var group = new THREE.Group();
-
 //text
 var load2 = new THREE.FontLoader();
-
 let newMaterial, Standard, newStandard;
-
+//stuff for delaying renderings and such
 const mixers = [];
 const clock = new THREE.Clock();
-
-
+//initial function that calls most of the necessary functions, 
 function init() {
-
+  //creates the container with an associated div tag, scene and calls the functions necessary to make the piece run
   container = document.querySelector( '#scene-container' );
-
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0x8FBCD4 );
-
   createCamera();
   createControls();
   createLights();
@@ -48,65 +39,42 @@ function init() {
   //loadModels();
   createSkybox();
   createRenderer();
-
   // play_audio(); 
-
   renderer.setAnimationLoop( () => {
-
     update();
     render();
-
   } );
-
 }
+//creates the skybox of an effected 360 picture in Big Sur
   function createSkybox(){
     scene.background = new THREE.CubeTextureLoader()					
     .setPath( 'js/THREE.js-master/src/examples/textures/cube/rock/' )					
     .load( [ 'px1.png', 'nx1.png', 'py1.png', 'ny1.png', 'pz1.png', 'nz1.png' ] );
   }
-
-
-
+//creates the camera
 function createCamera() {
-
   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 10000 );
   camera.position.set( 20, -1, -46);
-
 }
-
+//allows the user to move around the scene
 function createControls() {
-
   controls = new THREE.OrbitControls( camera, container );
-
 }
-
-
+//lights up the scene
 function createLights() {
-
   const ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
-
   const mainLight = new THREE.DirectionalLight( 0xffffff, 5 );
   mainLight.position.set( 10, 10, 10 );
-
   scene.add( ambientLight, mainLight );
-
 }
-
+//adds the text variables and adds the text, creates the sphere and adds it to the scene 
 function createMaterials(){
-
      let diffuseColor = "#9E4300";
      newMaterial = new THREE.MeshBasicMaterial( { color: "#9E4300", skinning: true} );
      Standard = new THREE.MeshStandardMaterial( { color: "#9E4300", skinning: true} );
-
      var imgTexture = new THREE.TextureLoader().load( "textures/water.JPG" );
      				imgTexture.wrapS = imgTexture.wrapT = THREE.RepeatWrapping;
      				imgTexture.anisotropy = 16;
-
-
-      
-
-//from Reddit
-
 load2.load( 'js/THREE.js-master/src/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
 	var textGeo = new THREE.TextGeometry( "Take Time", {
         font: font,
@@ -121,7 +89,6 @@ load2.load( 'js/THREE.js-master/src/examples/fonts/helvetiker_regular.typeface.j
     mesh.position.set(-0.5, -1, 2.3);
     scene.add( mesh );
 } );
-//
 
      newStandard = new THREE.MeshStandardMaterial( {
 										map: imgTexture,
@@ -136,10 +103,6 @@ load2.load( 'js/THREE.js-master/src/examples/fonts/helvetiker_regular.typeface.j
                     displacementscale: 0.1,
                     skinning: true
                   } );
-                  
-
-
-                  // console.log(sphere_one);
                   sphere_one.position.x = 0;
                   sphere_one.position.y = 0;
                   sphere_one.position.z = 0;
@@ -148,14 +111,8 @@ load2.load( 'js/THREE.js-master/src/examples/fonts/helvetiker_regular.typeface.j
                   scene.add( group );
   
                   var prevFog = true;
-
-      
-        
-
-
-
 }
-
+//audio functions that are called via their associated buttons, 3 tracks of audio that are repeated
 function play_audio1() {
 
   mediaElement1.play();
@@ -186,13 +143,11 @@ function play_audio6() {
   mediaElement6.play();
   audio.setMediaElementSource( mediaElement6 );
 }
-
+//adds a gltf to the scene (not currently being used in Under a Rock as of 12/8)
 function loadModels() {
 
   const loader = new THREE.GLTFLoader();
 
-  // A reusable function to set up the models. We're passing in a position parameter
-  // so that they can be individually placed around the scene
   const onLoad = ( gltf, position, material ) => {
 
     const model = gltf.scene.children[ 0 ];
@@ -221,7 +176,7 @@ function loadModels() {
 
   };
 
-  // the loader will report the loading progress to this function
+//in case there's any error's or slow loading the console lets us know
   const onProgress = () => {};
 
   // the loader will send any error messages to this function, and we'll log
@@ -240,7 +195,7 @@ function loadModels() {
   //loader.load( 'models/Stork.glb', gltf => onLoad( gltf, storkPosition ), onProgress, onError );
 
 }
-
+//creates a renderer
 function createRenderer() {
 
   // create a WebGLRenderer and set its width and height
@@ -259,78 +214,38 @@ function createRenderer() {
   container.appendChild( renderer.domElement );
 
 }
-
+//updating the clock so that things can be delayed
 function update() {
-
   const delta = clock.getDelta();
-
-  // /*for ( const mixer of mixers ) {
-  //
-  //   mixer.update( delta );
-  // }
-  // */
-
 }
-
+//renders the scene, primarily used to listen to the audio that is being heard by the analyser and turn it into a number that can 
 function render() {
-
-  //console.log(camera.position);
-
-
-  analyser.getFrequencyData();
-
-  // console.log(analyser);
-
-  // requestAnimationFrame( render );
-  //                   noise.seed(Math.random());
+ analyser.getFrequencyData();
                     var sum = analyser.data.reduce(function(a,b){return a+b;});
-                    var avg = sum/analyser.data.length;
-
-                    // console.log(camera.position);
-
-                    // group.rotation.y += avg/900;
-
+                    var avg = sum/analyser.data.length;                   
                      sphere_one.geometry.vertices.forEach(function(i){
                          var noisy = noise.simplex3(i.x,i.y,i.z)*0.00003;
                          i.x+=noisy*avg;
                          i.y+=noisy*avg;
                          i.z+=noisy*avg;
-
                      })
-
                         sphere_one.geometry.verticesNeedUpdate = true;
-
-                        // camera.position += avg;
-
-
-
-                  //    textGeo.geometry.vertices.forEach(function(i){
-                  //     var noisy = noise.simplex3(i.x,i.y,i.z)*0.00005;
-                  //     i.x+=noisy*avg;
-                  //     i.y+=noisy*avg;
-                  //     i.z+=noisy*avg;
-
-                  // })
-
-                  //    textGeo.geometry.verticesNeedUpdate = true;
-
-
+                      // camera.rotation +=0.5*avg
+                       
                         renderer.render( scene, camera );
 
 
 }
-
+//so that it appears okay on mobile and such
 function onWindowResize() {
 
   camera.aspect = container.clientWidth / container.clientHeight;
 
-  // update the camera's frustum
   camera.updateProjectionMatrix();
 
   renderer.setSize( container.clientWidth, container.clientHeight );
 
 }
-
 window.addEventListener( 'resize', onWindowResize );
-
+//call the initial function and therefore the whole scene
 init();
